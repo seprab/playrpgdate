2307bc16c2437b8ec67d53cce78c70267ca5033c
Sergio Prada <sergio.prada@sergio.prada-0LHM>
2024-06-16 13:30:58 -0500

    Added all instructions to build the game using the bootstrap.sh script

---
+++ bootstrap.sh
@@ -0,0 +1,70 @@
+#!/bin/bash
+export PROJECT_NAME="HelloWorld"
+
+
+echo "Select build option:"
+echo "1) Build for Playdate device"
+echo "2) Build for simulator"
+echo "3) Clean build directory"
+read -p "Option (1-3): " option
+
+# Navigate to the project root directory
+cd "$(dirname "$0")"
+
+clean_build_cache() {
+    # Check if the "build" directory exists before attempting to remove it
+    if [ -d "build" ]; then
+        rm -rf build
+        echo "Build directory removed."
+    fi
+
+    # Check if the "source" directory exists before attempting to remove it
+    if [ -d "source" ]; then
+        rm -rf source
+        echo "Source directory removed."
+    fi
+
+    # Check if the "HelloWorld.pdx" file exists before attempting to remove it
+    if [ -f "HelloWorld.pdx" ]; then
+        rm -rf HelloWorld.pdx
+        echo "HelloWorld.pdx file removed."
+    fi
+
+    echo "Build cache cleaned."
+    }
+
+case $option in
+    1)
+        # Clean previous build cache
+        clean_build_cache
+        # Build for Playdate device
+        mkdir -p build
+        echo "Configuring the project, specifying the source and build directories, and sets the toolchain file for cross-compilation."
+        cmake -S . -B build -DPROJECT_NAME=$PROJECT_NAME -DCMAKE_TOOLCHAIN_FILE=/Users/sergio.prada/Developer/PlaydateSDK/C_API/buildsupport/arm.cmake
+        echo "Compiling the project using the configuration specified in the build directory."
+        # using this command instead of "make" to avoid moving to another directory
+        cmake --build build
+        echo "Running the executable."
+        open -a "${PLAYDATE_SDK_PATH}/bin/Playdate Simulator.app" --args ${PROJECT_NAME}.pdx
+        ;;
+    2)
+        # Clean previous build cache
+        clean_build_cache
+        # Build for simulator
+        mkdir -p build
+        echo "Configuring the project, specifying the source and build directories, and sets the toolchain file for cross-compilation."
+        cmake -S . -B build -DPROJECT_NAME=$PROJECT_NAME
+        echo "Compiling the project using the configuration specified in the build directory."
+        # using this command instead of "make" to avoid moving to another directory
+        cmake --build build
+        echo "Running the executable."
+        open -a "${PLAYDATE_SDK_PATH}/bin/Playdate Simulator.app" --args ${PROJECT_NAME}.pdx
+        ;;
+    3)
+        # Clean previous build cache
+        clean_build_cache
+        ;;
+    *)
+        echo "Invalid option."
+        ;;
+esac
--- readme.md
+++ readme.md
@@ -1,13 +1,35 @@
 # Playdate-C++
 
-## Building for simulator
-1. mkdir build
-2. cd build
-3. cmake ..
-4. make
+## How to build
+Add execution permission to bootstrap with chmod +x bootstrap.sh, and launch it. Then choose the option that works for you.
 
-## Building for playdate
-1. mkdir build
-2. cd build
-3. cmake -DCMAKE_TOOLCHAIN_FILE=/Users/sergio.prada/Developer/ ..PlaydateSDK/C_API/buildsupport/arm.cmake
-4. make
+## Disclaimer
+
+This repository incorporates the submodule [playdate-cpp](https://github.com/nstbayless/playdate-cpp). Please refer to its README for guidance on configuration and setup. This project has been developed and tested on a Mac M1, running Sonoma 14.15.
+
+## Additional requirements to the official guide
+- PLaydate SDK path Environment variable is required!
+
+## Troubleshooting
+
+### Linker Issue on Mac
+
+**Problem Description:**
+
+When compiling, you might encounter a linker error similar to the following:
+```
+ld: error: .data has both ordered ['.ARM.exidx.text.__aeabi_atexit' ...] and unordered ['.data.__atexit_recursive_mutex' ...] sections.
+```
+
+This issue is discussed in detail in this [Playdate Developer Forum thread](https://devforum.play.date/t/cpp-guide-c-on-playdate/5085/39).
+
+**Solution:**
+
+The resolution involves updating to the latest ARM toolchain and creating symbolic links in `/usr/local/bin`. This solution is based on the advice provided in [this forum post](https://devforum.play.date/t/cpp-guide-c-on-playdate/5085/40).
+
+To apply the fix, execute the following command in the Terminal:
+
+```
+sudo ln -sf /Applications/ARM/bin/* /usr/local/bin
+```
+This command creates symbolic links for the ARM toolchain binaries in /usr/local/bin, ensuring that the build process can correctly locate and use these tools.
